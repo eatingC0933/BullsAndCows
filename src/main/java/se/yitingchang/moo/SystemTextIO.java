347cd21f32847255efa518334ac1e6b217215ac6
Yiting Chang <yiting.chang@iths.se>
2025-02-14 07:05:00 +0100

    Update SystemTextIO.java

--- src/main/java/se/yitingchang/moo/SystemTextIO.java
+++ src/main/java/se/yitingchang/moo/SystemTextIO.java
@@ -1,4 +1,39 @@
 package se.yitingchang.moo;
 
-public class SystemTextIO {
+import java.util.Scanner;
+
+public class SystemTextIO implements TextIO {
+    private final Scanner scanner;
+
+    public SystemTextIO() {
+        scanner = new Scanner(System.in);
+    }
+
+
+    @Override
+    public String getString() {
+        return scanner.nextLine();
+    }
+
+    @Override
+    public void addString(String s) {
+        System.out.println(s);;
+    }
+
+    @Override
+    public boolean yesNo(String prompt) {
+        System.out.print(prompt + " (yes/no): ");
+        String response = scanner.nextLine().trim().toLowerCase();
+        return response.equals("yes") || response.equals("y");
+    }
+
+    @Override
+    public void clear() {
+        System.out.println("Sudda sudda");
+    }
+
+    @Override
+    public void exit() {
+        System.exit(0);
+    }
 }
