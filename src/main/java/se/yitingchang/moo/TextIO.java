b4af103ba5eccdd7cabb8984ac9bec23267ba6ad
Yiting Chang <yiting.chang@iths.se>
2025-02-14 07:04:17 +0100

    Update TextIO.java

--- src/main/java/se/yitingchang/moo/TextIO.java
+++ src/main/java/se/yitingchang/moo/TextIO.java
@@ -1,4 +1,9 @@
 package se.yitingchang.moo;
 
 public interface TextIO {
+    String getString();
+    void addString(String s);
+    boolean yesNo(String prompt);
+    void clear();
+    void exit();
 }
