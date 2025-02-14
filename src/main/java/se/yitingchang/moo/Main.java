fbd16ae17e70476d056b5c038d1aa251359d0b7a
Yiting Chang <yiting.chang@iths.se>
2025-02-14 07:02:43 +0100

    Update Main.java

--- src/main/java/se/yitingchang/moo/Main.java
+++ src/main/java/se/yitingchang/moo/Main.java
@@ -1,4 +1,18 @@
 package se.yitingchang.moo;
 
+
+import java.sql.SQLException;
+
 public class Main {
+    public static void main(String[] args) {
+        try{
+            TextIO io= new SimpleWindow("Bulls and Cows");
+            DatabaseManager dbManager=new DatabaseManager();
+            MooController controller = new MooController(io, dbManager);
+            controller.start();
+        }catch (SQLException | InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
 }
